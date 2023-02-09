6540b3bd6a9b69aa5a78f0ba1076119ec7c5b1d3
qwerty6688 <120338259+qwerty6688@users.noreply.github.com>
2023-02-08 16:02:43 -0800

    Update SetNoticeGreen.js

--- notice/SetNoticeGreen.js
+++ notice/SetNoticeGreen.js
@@ -1,3 +1,4 @@
+/*
 var options = {
   text: "Welcome to my website!",
   gravity: "top",
@@ -12,3 +13,18 @@ var options = {
     background: "linear-gradient(to right, #00b09b, #96c93d)",
   }
 };
+*/
+Toastify({
+  text: "welcome to my website",
+  duration: 3000,
+  //destination: "https://github.com/apvarun/toastify-js",
+  //newWindow: true,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "linear-gradient(to right, rgba(0,200,50,0.9), rgba(0,100,5,0.9))",
+  },
+  onClick: function(){} // Callback after click
+}).showToast();
